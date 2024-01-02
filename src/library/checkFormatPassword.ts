e00e829b688581a9f0ff399f06871d3ed3b607c7
Roberto Silva <c.roberto.u@gmail.com>
2024-01-02 18:34:15 -0300

    feat: allow check the format

---
+++ src/library/checkFormatPassword.ts
@@ -0,0 +1,84 @@
+import { TConfig } from "../types/index.types";
+
+/**
+ * Checks if a given password meets the specified format
+ * requirements and returns whether it is valid or not, along with any error messages.
+ * @param {string} password - Represent the password that needs to be checked
+ * @param {TConfig} config - Represent the configuration object that contains the format requirements
+ * @returns {object} - Returns an `object` with two properties: isValid and message.
+ * - `isValid` is a `boolean` that represents whether the password is valid or not.
+ * - `message` is a `string` or an `array` of strings that contains any error messages.
+ */
+const checkFormatPassword = (password: string, config: TConfig) => {
+  if (!config.length) {
+    config.length = {
+      min: {
+        value: 8,
+      },
+      max: {
+        value: 64,
+      },
+    };
+  }
+
+  const messages = [];
+
+  if (config.length && password.length < config.length.min.value) {
+    messages.push(config.length?.min.message || "Password is too short.");
+  }
+
+  if (config.length && password.length > config.length.max.value) {
+    messages.push(config.length?.max.message || "Password is too long.");
+  }
+
+  if(config.string?.allow === false && /[a-zA-Z]/.test(password)) {
+    messages.push(
+      config.string?.message || "Password must not contain any letters."
+    );
+  }
+
+  if (config.string?.allow !== false && config.string?.upperCase.allow !== false && !/[A-Z]/.test(password)) {
+    messages.push(
+      config.string?.upperCase.message ||
+        "Password must contain at least one uppercase letter."
+    );
+  }
+
+  if (config.string?.allow !== false && config.string?.lowerCase.allow !== false && !/[a-z]/.test(password)) {
+    messages.push(
+      config.string?.lowerCase.message ||
+        "Password must contain at least one lowercase letter."
+    );
+  }
+
+  if (config.number?.allow !== false && !/\d/.test(password)) {
+    messages.push(
+      config.number?.message || "Password must contain at least one number."
+    );
+  }
+
+  if(config.number?.allow === false && /\d/.test(password)) {
+    messages.push(
+      config.number?.message || "Password must not contain any numbers."
+    );
+  }
+
+  if (config.specialChar?.allow !== false && !/[\\W_]/.test(password)) {
+    messages.push(
+      config.specialChar?.message ||
+        "Password must contain at least one special character."
+    );
+  }
+
+  if(config.specialChar?.allow === false && /[\\W_]/.test(password)) {
+    messages.push(
+      config.specialChar?.message || "Password must not contain any special characters."
+    );
+  }
+
+  const message = messages.length === 0 ? "Password is valid." : messages;
+
+  return { isValid: messages.length === 0, message };
+};
+
+export default checkFormatPassword;
--- src/types/index.types.ts
+++ src/types/index.types.ts
@@ -1,9 +1,89 @@
-/**
- * ADD HERE YOUR OWN TYPES
- */
-export type Config = {
-  attribute1: string;
-  attribute2: number;
-  attribute3?: object[];
-  attribute4?: number;
-}; 
+export type TConfig = {
+  /**
+   * The minimum and maximum length of the password.
+   */
+  length?: {
+    /**
+     * The minimum length of the password.
+     */
+    min: {
+      /**
+       * Value of the minimum length of the password.
+       */
+      value: number;
+      /**
+       * Custom message to display when the password is too short.
+       */
+      message?: string;
+    };
+    /**
+     * The maximum length of the password.
+     */
+    max: {
+      /**
+       * Value of the maximum length of the password.
+       */
+      value: number;
+      /**
+       * Custom message to display when the password is too long.
+       */
+      message?: string;
+    };
+  };
+  /**
+   * The allowed characters in the password.
+   */
+  string?: {
+    allow?: boolean;
+    /**
+     * The allowed uppercase characters in the password.
+     */
+    message?: string;
+    upperCase: {
+      /**
+       * The allowed uppercase characters in the password.
+       */
+      allow?: boolean;
+      /**
+       * Custom message to display when the password does not contain an uppercase letter.
+       */
+      message?: string;
+    };
+    lowerCase: {
+      /**
+       * The allowed lowercase characters in the password.
+       */
+      allow?: boolean;
+      /**
+       * Custom message to display when the password does not contain a lowercase letter.
+       */
+      message?: string;
+    };
+  };
+  /**
+   * The allowed numbers in the password.
+   */
+  number?: {
+    /**
+     * The allowed numbers in the password.
+     */
+    allow?: boolean;
+    /**
+     * Custom message to display when the password does not contain a number.
+     */
+    message?: string;
+  };
+  /**
+   * The allowed special characters in the password.
+   */
+  specialChar?: {
+    /**
+     * The allowed special characters in the password.
+     */
+    allow?: boolean;
+    /**
+     * Custom message to display when the password does not contain a special character.
+     */
+    message?: string;
+  };
+};
